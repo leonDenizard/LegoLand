534496999320db2c0a2a9085161b1e80ab8be0e4
leon <de.nizard@hotmail.com>
2023-01-05 14:22:47 -0300

    Inserido função para capturar o valor do Range price

--- script_slider_range.js
+++ script_slider_range.js
@@ -1,8 +1,14 @@
 const slider = document.querySelector('.range');
+const price = document.getElementById('price');
+
+price.innerText = slider.value;
+slider.oninput = function(){
+    price.innerText = this.value;
+}
 
 slider.addEventListener('mousemove', function(){
     let x = slider.value;
-    let color = `linear-gradient(90deg, #ef233c ${x}%, #e5e5e5 ${x}%)`
+    let color = `linear-gradient(90deg, #ef233c ${x*0.1}%, #e5e5e5 ${x*0.1}%)`
                 
     slider.style.background = color;
     
